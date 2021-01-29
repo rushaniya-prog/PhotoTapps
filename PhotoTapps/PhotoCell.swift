e04b60129eae9e46bca27a4488fffee23ab79a7c
Рушания <bashkurovar@mail.ru>
2021-01-29 18:45:12 +0300

    first commit

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Рушания on 27.01.2021.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+    
+}
